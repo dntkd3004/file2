03b81a5bf3dba261ec32dafbae9b488559929ab9
dntkd3004 <dldntkd12@gmail.com>
2020-11-09 12:52:10 +0900

    커밋메세지

--- src/java_board/App.java
+++ src/java_board/App.java
@@ -56,7 +56,7 @@ public class App {
 			}
 			if(cmd.equals("list")) {
 				ArrayList<Article> articles = articleDao.getArticles();
-				printArticles(articles);
+				printArticles(articles, 1);
 			}
 			if(cmd.equals("update")) {
 				
@@ -161,7 +161,6 @@ public class App {
 						} if(readCmd == 5) {
 							break;
 						}
-
 					}
 				}
 			}
@@ -174,7 +173,7 @@ public class App {
 				
 				searchedArticles = articleDao.getSearchedArticlesByFlag(flag, keyword);
 
-				printArticles(searchedArticles);
+				printArticles(searchedArticles, 1);
 			}
 			if(cmd.equals("sort")) {
 				System.out.println("정렬 대상을 선택해주세요. (like : 좋아요, hit : 조회수) :");
@@ -187,10 +186,21 @@ public class App {
 				//조회수로 오름차순
 				ArrayList<Article> articles = articleDao.getArticles();
 				Collections.sort(articles, comp);
-				printArticles(articles);
+				printArticles(articles, 1);
 			}
 			if(cmd.equals("page")) {
-				System.out.println("(prev : 이전, next : 다음, go : 선택, back : 뒤로가기)");
+				ArrayList<Article> articles = articleDao.getArticles();
+				int currentPageNo = 1;
+				printArticles(articles, currentPageNo);
+				while(true) {
+					String pageCmd = sc.nextLine();
+					if(pageCmd.equals("next")) {
+						currentPageNo++;
+						printArticles(articles, currentPageNo);
+					} else if(pageCmd.equals("back")) {
+						break;
+					}
+				}
 			}
 			if(cmd.equals("signup")) {
 				System.out.println("======== 회원가입을 진행합니다.========");
@@ -243,8 +253,45 @@ public class App {
 			}
 		}	
 	}
-	private void printArticles(ArrayList<Article> articleList) {
-		for (int i = 0; i < articleList.size(); i++) {
+	private void printArticles(ArrayList<Article> articleList, int currentPageNo) {
+		// 현재 페이지
+		int totalCntOfItems = articles.size(); // 전체 게시물 개수
+		int startPageNo = 1; // 시작 페이지 번호
+		int itemsCntPerPage = 3; // 페이지당 출력 게시물 개수
+		int pageCntPerBlock = 5; // 한 페이지 블록 당 페이지 개수
+		int endPageNo = (int)Math.ceil((double)totalCntOfItems / itemsCntPerPage); // 마지막 페이지 번호
+		
+		// 현재 페이지가 시작페이지보다 작으면 안됨
+		if (currentPageNo < startPageNo) {
+			currentPageNo = startPageNo;
+		}
+
+		// 현재 페이지가 마지막페이지보다 크면 안됨
+		if (currentPageNo > endPageNo) {
+			currentPageNo = endPageNo; 
+		}
+		
+		int currentPageBlock = (int)Math.ceil((double)currentPageNo / pageCntPerBlock) ; // 현재 페이지 블록
+		int startPageNoInBlock = (currentPageBlock - 1) * pageCntPerBlock + 1 ; // 현재 페이지 블록의 시작 페이지 번호
+		int endPageNoInBlock = startPageNoInBlock + pageCntPerBlock - 1;// // 현재 페이지 블록의 마지막 페이지 번호
+
+		// 페이지 번호가 마지막 페이지를 넘으면 안됨
+		if(endPageNoInBlock > endPageNo) {
+			endPageNoInBlock = endPageNo;
+		}
+		
+		// 해당 페이지의 게시물 목록의 첫 인덱스
+		int startIndex = (currentPageNo - 1) * itemsCntPerPage;
+		
+		// 해당 페이지의 게시물 목록의 마지막 인덱스
+		int endIndex = startIndex + itemsCntPerPage;
+		
+		// 페이지의 마지막 인덱스가 저장소의 마지막 인덱스보다 크면 안됨
+		if(endIndex > totalCntOfItems) {
+			endIndex = totalCntOfItems;
+		}
+		
+		for (int i = 0; i < endIndex; i++) {
 			Article article = articleList.get(i);
 			System.out.println("번호 : " + article.getId());
 			System.out.println("제목 : " + article.getTitle());
@@ -253,8 +300,19 @@ public class App {
 			System.out.println("작성자 : " + regMember.getNickname());
 			System.out.println("조회수 : " + article.getHit());
 			int likeCnt = likeDao.getLikeCount(article.getId());
-			System.out.println("좋아요 : " + likeCnt);
+			System.out.println("좋아요 : " + article.getLikeCnt());
 			System.out.println("===================");
+		}
+		
+		// 페이지 블록 출력
+		for(int i = startPageNoInBlock ; i <= endPageNoInBlock; i++ ) {
+					
+			if(i == currentPageNo) {
+				System.out.print("[" + i+ "] ");
+			} else {
+				System.out.print(i+ " ");
+			}
+			System.out.println(" ");
 		}
 	}
 	private void printReplies(ArrayList<Reply> replyList) {
@@ -276,7 +334,7 @@ public class App {
 		System.out.println("작성자 :" + regMember.getNickname());
 		System.out.println("조회수 :" + target.getHit());
 		int likeCnt = likeDao.getLikeCount(target.getId());
-		System.out.println("좋아요 : " + likeCnt);
+		System.out.println("좋아요 : " + target.getLikeCnt());
 		System.out.println("===============");
 		System.out.println("================댓글==============");
 		
@@ -301,6 +359,7 @@ public class App {
 
 		return true;
 	}
+	
 }
 class MyComparator implements Comparator<Article> {
 
